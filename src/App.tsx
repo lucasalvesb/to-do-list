e0f04b1e0bf8443a3310019ca18fa88c83097efa
Lucas Alves <lucasalves94@gmail.com>
2023-01-14 02:08:59 -0300

    trying to fix a bug with the right word bleeding through the box

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import AddTask from './components/AddTask'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <AddTask />
+      <Tasks />
     </div>
   )
 }
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,15 @@
+import './Tasks.css'
+
+export default function Tasks() {
+  return (
+    <div className="tasks-list-container">
+      <header className="tasks-list-header">
+      <p className="created-tasks">Tarefas criadas <span>0</span></p>
+      <p className="concluded-tasks">Concluídas <span>0</span></p>
+      </header>
+      <main className="tasks-list-main">
+        
+      </main>
+    </div>
+  )
+}
