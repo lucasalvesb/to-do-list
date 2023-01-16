bb1e21e05511d9f912833fee44dc62cdfb57020d
Lucas Alves <lucasalves94@gmail.com>
2023-01-16 00:19:07 -0300

    fixed multiple structure problems and typescript problems after checking challenge resolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,72 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useEffect, useState } from 'react'
+import { Header } from './components/Header/Header'
+import { Tasks } from './components/Tasks/Tasks'
 
+const LOCAL_STORAGE_KEY = 'todo:savedTasks'
 
-
-const tasks = [
-  {
-    id: 1,
-    content: [
-    {type: 'paragraph', content: 'Hello, guys!' }, 
-    {type: 'link', content: 'https://github.com/lucasalvesb' }
-    ]
-  },]
-
-
+export interface ITask {
+  id: string
+  title: string
+  isCompleted: boolean
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
 
+  function loadSavedTasks() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (saved) {
+      setTasks(JSON.parse(saved))
+    }
+  }
+
+  useEffect(() => {
+    loadSavedTasks()
+  }, [])
+
+  function setTasksAndSave(newTasks: ITask[]) {
+    setTasks(newTasks)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks))
+  }
+
+  function addTask(taskTitle: string) {
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      },
+    ])
+  }
+
+  function deleteTaskById(taskId: string) {
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    setTasksAndSave(newTasks)
+  }
+
+  function toggleTaskCompletedById(taskId: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        }
+      }
+      return task
+    })
+    setTasksAndSave(newTasks)
+  }
 
   return (
-    <div className="App">
-      <Header />
-      <Tasks />
-    </div>
+    <>
+      <Header onAddTask={addTask} />
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onComplete={toggleTaskCompletedById}
+      />
+    </>
   )
 }
 
